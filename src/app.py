d4e0f1ef268bd290fb23e86f0f1ff3b999a8f5e8
Felixnite <141184414+Felixnite@users.noreply.github.com>
2024-01-16 06:42:06 +0000

    first endpoint

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,16 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# Get all Characters
+
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    characters = Characters.query.all()
+    characters = list(map(lambda x: x.serialize(), characters))
+    return jsonify(characters), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,9 +7,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
@@ -17,3 +18,63 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(80), nullable=False)
+    hair_color = db.Column(db.String(80), nullable=False)
+    gender = db.Column(db.String(80), nullable=False)
+    favorites = db.relationship('Favorites', backref='characters', lazy=True)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color,
+            "gender": self.gender,
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    population = db.Column(db.String(80), nullable=False)
+    diameter = db.Column(db.String(80), nullable=False)
+    gravity = db.Column(db.String(80), nullable=False)
+    weather = db.Column(db.String(80), nullable=False)
+    favorites = db.relationship('Favorites', backref='planets', lazy=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "weather": self.weather,
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=False)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "characters_id": self.characters_id,
+            "planets_id": self.planets_id,
+        }    
